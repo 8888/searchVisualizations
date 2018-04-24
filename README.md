aa955330db7e906291c5db37abc9792838774c9a
betterin30days <leecostello1@gmail.com>
2018-04-23 22:45:35 -0400

    created initial drawing of binary search

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 Visualizations of common computer science search algorithms. Searches are shown using HTML5 canvas elements and logic is in JavaScript.
 
 # Local Development
-ES6 modules are used to import/export code and most browsers now support this without the use of any bundling library. Since local development runs into CORS issues when trying to import from the file system, local files need to be served by a simple web server.
+ES6 modules are used to import/export code and most modern browsers now support this without the use of any bundling library. Since local development runs into CORS issues when trying to import from the file system, local files need to be served by a simple web server.  
+Once a web server is running, go to [localhost:8000](http://localhost:8000/).
 
-### PHP
+To start a web server using PHP:
 ```shell
 $ php -S 127.0.0.1:8000
 ```
-Credit and MANY more ways to start a webserver: [willurd](https://gist.github.com/willurd/5720255)
+Web server one-liner credit and MANY more ways to start a webserver: [willurd](https://gist.github.com/willurd/5720255)
 
 There are currently no dependencies as this is written in vanilla js and avoids bundling. Testing is also web-based, and the browser loads the packages from [unpkg](https://unpkg.com/#/) as scripts.
 
--- index.js
+++ index.js
@@ -11,17 +11,75 @@ const canvasHeight = canvas.height;
 const canvasBounds = canvas.getBoundingClientRect();
 const ctx = canvas.getContext('2d');
 
-const searchField = [8,13,19,22,27,35,42,49,55,58,60,73,79,88,94,101];
+// temporary values
+const searchField = [8,13,22,27,35,42,49,55,58,60,73,79,88,94,101];
 const searchTarget = 88;
+const searchPath = binarySearch(searchField, searchTarget);
 
-function init() {
-    console.log(binarySearch(searchField, searchTarget));
-    console.log(binarySearch([0,1,2,3,4,5,6,7,8,9], -1));
-}
+function init() {}
 
 function update(delta) {}
 
-function display() {}
+function display() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.fillStyle = '#000000'; // black
+    ctx.strokeStyle = '#000000';
+    const fontSize = 12;
+    ctx.font = fontSize.toString + 'px Verdana';
+    ctx.textAlign = 'start';
+    ctx.lineWidth = 2;
+
+    const margin = 25;
+
+    ctx.fillText(
+        'Search Target: ' + searchTarget.toString(),
+        margin,
+        margin
+    );
+
+    // draw the number field
+    const y = 300;
+    const spacing = 40;
+    for (let i = 0; i < searchField.length; i++) {
+        ctx.textAlign = 'center';
+        const x = margin + (i * spacing);
+        ctx.fillText(searchField[i], x, y);
+    }
+
+    // draw the search path
+    let lineOriginX = margin + searchPath[0] * spacing; // center number is first
+    let lineOriginY = 100;
+    for (let i = 0; i < searchPath.length; i++) {
+        // searchPath is an array of indexes
+        const x = margin + searchPath[i] * spacing; // x of number
+        // draw line path
+        // horizontal
+        ctx.strokeStyle = '#33cc33'; // green
+        ctx.beginPath();
+        ctx.moveTo(lineOriginX, lineOriginY + (i * spacing));
+        ctx.lineTo(x, lineOriginY + (i * spacing));
+        ctx.stroke();
+        lineOriginX = x;
+        // vertical part 1
+        ctx.beginPath();
+        ctx.moveTo(x, lineOriginY + (i * spacing));
+        ctx.lineTo(x, lineOriginY + (spacing * (i + 1))); // center is 1/2 fontSize and radius is fontSize
+        ctx.stroke();
+        // change color if this is an incorrect path
+        ctx.strokeStyle = searchField[searchPath[i]] == searchTarget ? '#33cc33' : '#cc0000'; // red
+        // vertical part 2
+        ctx.beginPath();
+        ctx.moveTo(x, lineOriginY + (spacing * (i + 1)));
+        ctx.lineTo(x, y - (fontSize * 1.5)); // center is 1/2 fontSize and radius is fontSize
+        ctx.stroke();
+        // draw outline circle
+        ctx.beginPath();
+        ctx.arc(x, y - (fontSize/2), fontSize, 0, 2 * Math.PI);
+        // this centers the circle on the font
+        ctx.stroke();
+    }
+}
 
 window.onload = function() {
     init();
