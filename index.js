c7565b6ae3f8ead13d092e0c66a467cb684b7e39
betterin30days <leecostello1@gmail.com>
2018-04-24 22:03:28 -0400

    added user input box for search target

--- index.js
+++ index.js
@@ -11,32 +11,82 @@ const canvasHeight = canvas.height;
 const canvasBounds = canvas.getBoundingClientRect();
 const ctx = canvas.getContext('2d');
 
+// drawing sizes
+const margin = 25;
+const fontSize = 12;
+
+// mouse location
+let mouseX = null;
+let mouseY = null;
+
 // temporary values
 const searchField = [8,13,22,27,35,42,49,55,58,60,73,79,88,94,101];
-const searchTarget = 88;
-const searchPath = binarySearch(searchField, searchTarget);
+let searchTarget = 88;
+let searchPath = [];
 
-function init() {}
+// input box
+let searchTargetInputFocus = false;
 
-function update(delta) {}
+function init() {
+    // user input
+    canvas.addEventListener('mousemove', function(event) {
+        mouseX = event.clientX - canvasBounds.left;
+        mouseY = event.clientY - canvasBounds.top;
+    });
+
+    canvas.addEventListener('mousedown', function(event) {
+        if (event.button === 0) { // left click
+            // check if input box was clicked
+            if (
+                mouseX >= margin &&
+                mouseX <= margin + fontSize * 5 &&
+                mouseY >= margin + fontSize &&
+                mouseY <= margin + fontSize + fontSize * 1.5
+            ) {
+                searchTargetInputFocus = true;
+            } else {
+                searchTargetInputFocus = false;
+            }
+        }
+    });
+
+    canvas.addEventListener('keydown', function(event) {
+        if (searchTargetInputFocus) {
+            if (event.keyCode >= 48 && event.keyCode <= 57) {
+                // 0-9 is keycode 48-57
+                searchTarget = parseInt(searchTarget.toString() + (event.keyCode - 48).toString(), 10);
+            } else if (event.keyCode >= 96 && event.keyCode <= 105) {
+                // numpad 0-9 is keycode 96-105
+                searchTarget = parseInt(searchTarget.toString() + (event.keyCode - 96).toString(), 10);
+            } else if (event.keyCode == 8) {
+                // backspace
+                searchTarget = Math.floor(searchTarget / 10);
+            }
+        }
+    });
+}
+
+function update(delta) {
+    searchPath = binarySearch(searchField, searchTarget);
+}
 
 function display() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
     ctx.fillStyle = '#000000'; // black
     ctx.strokeStyle = '#000000';
-    const fontSize = 12;
     ctx.font = fontSize.toString + 'px Verdana';
     ctx.textAlign = 'start';
     ctx.lineWidth = 2;
 
-    const margin = 25;
-
+    // search target
     ctx.fillText(
         'Search Target: ' + searchTarget.toString(),
         margin,
         margin
     );
+    ctx.strokeRect(margin, margin + fontSize, fontSize * 5, fontSize * 1.5);
+    ctx.fillText(searchTarget.toString(), margin + fontSize / 2, (margin + fontSize) + (fontSize * 1.5) - (fontSize * 0.5)); // rectY + rectHeight - 1/2 font
 
     // draw the number field
     const y = 300;
