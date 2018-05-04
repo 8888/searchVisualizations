9685acd32328dc3398cbb26ef2c6cd09d5b4ceb2
betterin30days <leecostello1@gmail.com>
2018-05-04 11:25:27 -0400

    added more binary search tree insert tests, wrote bst insert function

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const node = (key, value) => {
+const node = (key, value, left = null, right = null) => {
     // creates the basic data structure used
     // key is the number to order by
     // if this was a sorted array:
@@ -8,9 +8,38 @@ const node = (key, value) => {
     // value would be the index
     // value is what is stored
     // l & r = left and right nodes
-    return {k: key, v: value, l: null, r: null};
+    return {k: key, v: value, l: left, r: right};
 };
 
-const insert = (node, tree) => {};
+const insert = (key, value, tree) => {
+    // inserts a node into a binary search tree
+    // returns the new tree
+    // uses recursion with each lower node being passed as tree
+    if (key == tree.k) {
+        // node key already exists
+        // replace value
+        return node(key, value, tree.l, tree.r);
+    } else if (key < tree.k) {
+        // move down the tree to the left
+        if (tree.l) {
+            // left node exists
+            // recursively call insert w/ left node
+            return node(tree.k, tree.v, insert(key, value, tree.l), tree.r);
+        } else {
+            // insert into left node
+            return node(tree.k, tree.v, node(key, value), tree.r);
+        }
+    } else {
+        // move down the tree to the right
+        if (tree.r) {
+            // right node exists
+            // recursively call insert w/ right node
+            return node(tree.k, tree.v, tree.l, insert(key, value, tree.r));
+        } else {
+            // insert into right node
+            return node(tree.k, tree.v, tree.l, node(key, value));
+        }
+    }
+};
 
 export {node, insert};
--- test/testBinarySearchTree.js
+++ test/testBinarySearchTree.js
@@ -2,22 +2,51 @@
 
 import * as bst from '../binarySearchTree.js';
 
-const tree1 = {
-    k: 5,
-    v: 5,
-    l: {k: 2, v: 2, l: null, r: null},
-    r: {k: 8, v: 8, l: null, r: null}
-};
-
 describe('binarySearchTree', function() {
     describe('node()', function() {
-        it('returns correct object: ', function() {
+        it('no left or right provided: ', function() {
             chai.expect(bst.node(1, 1)).eql({k: 1, v: 1, l: null, r: null});
+        });
+        it('left provided but not right: ', function() {
+            chai.expect(bst.node(
+                2,
+                2,
+                bst.node(1, 1)
+            )).eql({
+                k: 2,
+                v: 2,
+                l: {k: 1, v: 1, l: null, r: null},
+                r: null
+            });
+        });
+        it('right provided but not left: ', function() {
+            chai.expect(bst.node(
+                2,
+                2,
+                null,
+                bst.node(3, 3)
+            )).eql({
+                k: 2,
+                v: 2,
+                l: null,
+                r: {k: 3, v: 3, l: null, r: null}
+            });
         });
     });
     describe('insert()', function() {
+        let tree1 = {};
+        beforeEach(function() {
+            // tree should never be mutated, but a test checks that
+            // reset value to ensure all use same data
+            tree1 = {
+                k: 5,
+                v: 5,
+                l: {k: 2, v: 2, l: null, r: null},
+                r: {k: 8, v: 8, l: null, r: null}
+            };
+        });
         it('inserted node < all nodes: ', function() {
-            chai.expect(bst.insert(bst.node(1, 1), tree1)).eql({
+            chai.expect(bst.insert(1, 1, tree1)).eql({
                 k: 5,
                 v: 5,
                 l: {
@@ -30,7 +59,7 @@ describe('binarySearchTree', function() {
             });
         });
         it('inserted node > all nodes: ', function() {
-            chai.expect(bst.insert(bst.node(10, 10), tree1)).eql({
+            chai.expect(bst.insert(10, 10, tree1)).eql({
                 k: 5,
                 v: 5,
                 l: {k: 2, v: 2, l: null, r: null},
@@ -43,7 +72,7 @@ describe('binarySearchTree', function() {
             });
         });
         it('L node < inserted node < root node: ', function() {
-            chai.expect(bst.insert(bst.node(3, 3), tree1)).eql({
+            chai.expect(bst.insert(3, 3, tree1)).eql({
                 k: 5,
                 v: 5,
                 l: {
@@ -56,7 +85,7 @@ describe('binarySearchTree', function() {
             });
         });
         it('root node < inserted node < R node: ', function() {
-            chai.expect(bst.insert(bst.node(7, 7), tree1)).eql({
+            chai.expect(bst.insert(7, 7, tree1)).eql({
                 k: 5,
                 v: 5,
                 l: {k: 2, v: 2, l: null, r: null},
@@ -69,7 +98,41 @@ describe('binarySearchTree', function() {
             });
         });
         it('inserted key already exists: ', function() {
-            chai.expect(bst.insert(bst.node(8, 7), tree1)).eql(tree1);
+            chai.expect(bst.insert(8, 7, tree1)).eql({
+                k: 5,
+                v: 5,
+                l: {k: 2, v: 2, l: null, r: null},
+                r: {k: 8, v: 7, l: null, r: null}
+            });
+        });
+        it('inserted key exists, keeps children intact: ', function() {
+            chai.expect(bst.insert(5, 4, tree1)).eql({
+                k: 5,
+                v: 4,
+                l: {k: 2, v: 2, l: null, r: null},
+                r: {k: 8, v: 8, l: null, r: null}
+            });
+        });
+        it('does not mutate original tree: ', function() {
+            const newTree = bst.insert(1, 1, tree1);
+            chai.expect(newTree).not.eql(tree1);
+            chai.expect(tree1).eql({
+                k: 5,
+                v: 5,
+                l: {k: 2, v: 2, l: null, r: null},
+                r: {k: 8, v: 8, l: null, r: null}
+            });
+            chai.expect(newTree).eql({
+                k: 5,
+                v: 5,
+                l: {
+                    k: 2,
+                    v: 2,
+                    l: {k: 1, v:1, l: null, r: null},
+                    r: null
+                },
+                r: {k: 8, v: 8, l: null, r: null}
+            });
         });
     });
 });
