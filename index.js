412e7233855e4736db5c1216d1d48f871f09f3e1
betterin30days <leecostello1@gmail.com>
2018-06-01 18:23:05 -0400

    extracted drawing functions into draw.js, called by index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import * as bsa from './search/binarySearchAlgorithm.js';
 import * as bst from './search/binarySearchTree.js';
+import * as Draw from './view/draw.js';
 
 const mainCanvas = document.getElementById('mainCanvasElement');
 const sideCanvas = document.getElementById('sideCanvasElement');
@@ -19,8 +20,18 @@ const sideCanvasBounds = sideCanvas.getBoundingClientRect();
 const sideCtx = sideCanvas.getContext('2d');
 
 // drawing sizes
-const margin = 25;
-const fontSize = 10;
+const defaultStyle = Draw.styleParameters(
+    '#000000', // fillStyle - black
+    '#000000', // strokeStyle
+    2, // lineWidth
+    10, // fontSize
+    'Verdana', // fontName
+    'start', // textAlign
+    25, // xMargin
+    25, // yMargin
+    {arrayStart: 0, arrayValues: 40, bstOrigin: 360}, // xSpacing
+    {arrayCenter: 300, arrayLineOrigin: 100, bstOrigin: 360, bstLayer: 50} // ySpacing
+);
 
 // temporary values
 // let searchField = [8,13,22,27,35,42,49,55,58,60,73,79,88,94,101];
@@ -83,147 +94,57 @@ function update(delta) {
 function display() {
     // all drawing is currently very inefficient
     // portions are redrawn every frame even when they don't change
-    // functions are redefined every frame
     // this is purely prototype at this point just to see how it will look
     mainCtx.clearRect(0, 0, mainCanvasWidth, mainCanvasHeight);
     sideCtx.clearRect(0, 0, sideCanvasWidth, sideCanvasHeight);
+    Draw.updateCanvasStyle(mainCtx, defaultStyle);
+    Draw.updateCanvasStyle(sideCtx, defaultStyle);
 
-    // search target
-    sideCtx.fillStyle = '#000000'; // black
-    sideCtx.font = (fontSize * 1.5).toString() + 'px Verdana';
-    sideCtx.fillText(
-        'Search Target: ' + searchTarget.toString(),
-        2,
-        margin
+    Draw.drawStats(
+        sideCtx,
+        searchTarget,
+        bst.height(searchTree),
+        searchField.length,
+        step,
+        defaultStyle
     );
-    sideCtx.fillText(
-        'Tree Height: ' + bst.height(searchTree),
-        2,
-        margin * 2
+    Draw.updateCanvasStyle(sideCtx, defaultStyle);
+
+    Draw.drawArrayField(
+        mainCtx,
+        searchField,
+        defaultStyle.xSpacing['arrayStart'],
+        defaultStyle.ySpacing['arrayCenter'],
+        defaultStyle.xSpacing['arrayValues'],
+        defaultStyle
     );
-    sideCtx.fillText(
-        'Total Nodes: ' + searchField.length,
-        2,
-        margin * 3
+    Draw.updateCanvasStyle(mainCtx, defaultStyle);
+
+    Draw.drawSearchPath(
+        mainCtx,
+        searchField,
+        searchTarget,
+        searchPath,
+        step,
+        defaultStyle.xSpacing['arrayValues'],
+        defaultStyle.ySpacing['arrayLineOrigin'],
+        defaultStyle.ySpacing['arrayCenter'],
+        defaultStyle
     );
-    sideCtx.fillText(
-        'Current Search Step: ' + (step + 1),
-        2,
-        margin * 4
+    Draw.updateCanvasStyle(mainCtx, defaultStyle);
+
+    Draw.drawBst(
+        mainCtx,
+        searchTree,
+        step,
+        searchPath,
+        defaultStyle.xSpacing['bstOrigin'],
+        defaultStyle.ySpacing['bstOrigin'],
+        defaultStyle.ySpacing['bstLayer'],
+        bst.height(searchTree),
+        defaultStyle
     );
-
-    mainCtx.fillStyle = '#000000'; // black
-    mainCtx.strokeStyle = '#000000';
-    mainCtx.font = fontSize.toString() + 'px Verdana';
-    mainCtx.textAlign = 'start';
-    mainCtx.lineWidth = 2;
-
-    // draw the number field
-    const y = 300;
-    const spacing = 40;
-    for (let i = 0; i < searchField.length; i++) {
-        mainCtx.textAlign = 'center';
-        const x = margin + (i * spacing);
-        mainCtx.fillText(searchField[i], x, y);
-    }
-
-    // draw the search path
-    function drawSearchPath(i) {
-        // searchPath is an array of indexes
-        const x = margin + searchPath[i] * spacing; // x of number
-        // draw line path
-        // horizontal
-        mainCtx.strokeStyle = '#33cc33'; // green
-        mainCtx.beginPath();
-        mainCtx.moveTo(lineOriginX, lineOriginY + (i * spacing));
-        mainCtx.lineTo(x, lineOriginY + (i * spacing));
-        mainCtx.stroke();
-        lineOriginX = x;
-        // vertical part 1
-        mainCtx.beginPath();
-        mainCtx.moveTo(x, lineOriginY + (i * spacing));
-        mainCtx.lineTo(x, lineOriginY + (spacing * (i + 1))); // center is 1/2 fontSize and radius is fontSize
-        mainCtx.stroke();
-        // change color if this is an incorrect path
-        mainCtx.strokeStyle = searchField[searchPath[i]] == searchTarget ? '#33cc33' : '#cc0000'; // red
-        // vertical part 2
-        mainCtx.beginPath();
-        mainCtx.moveTo(x, lineOriginY + (spacing * (i + 1)));
-        mainCtx.lineTo(x, y - (fontSize * 1.5)); // center is 1/2 fontSize and radius is fontSize
-        mainCtx.stroke();
-        // draw outline circle
-        mainCtx.beginPath();
-        mainCtx.arc(x, y - (fontSize/2), fontSize, 0, 2 * Math.PI);
-        // this centers the circle on the font
-        mainCtx.stroke();
-    }
-
-    let lineOriginX = margin + searchPath[0] * spacing; // center number is first
-    let lineOriginY = 100;
-    if (step != -1) {
-        for (let i = 0; i <= step; i++) {
-            drawSearchPath(i);
-        }
-    }
-
-    // draw the binary search tree
-    function drawBst(tree, x, y, layerSpacing, height) {
-        // draw current node
-        mainCtx.strokeStyle = '#000000'; // black
-        mainCtx.lineWidth = 2;
-        mainCtx.fillText(tree.k, x, y);
-        mainCtx.beginPath();
-        if (step !== -1 && searchPath[step] === tree.v) {
-            // currently searching on this node
-            mainCtx.strokeStyle = '#0061ff'; // blue
-            mainCtx.lineWidth = 4;
-        }
-        mainCtx.arc(x, y, fontSize, 0, 2 * Math.PI);
-        mainCtx.stroke();
-        // reset values
-        mainCtx.strokeStyle = '#000000';
-        mainCtx.lineWidth = 2;
-        // parents draw lines to their children
-        // then recursively draw lower nodes
-        if (tree.l) {
-            mainCtx.beginPath();
-            mainCtx.moveTo(x - fontSize, y); // x - radius
-            mainCtx.lineTo(
-                x - layerSpacing - (Math.pow(5, height)), // childX center
-                y + layerSpacing - fontSize // childY - radius
-            );
-            mainCtx.stroke();
-            drawBst(
-                tree.l,
-                x - layerSpacing - (Math.pow(5, height)),
-                y + layerSpacing,
-                layerSpacing,
-                height - 1
-            );
-        }
-        if (tree.r) {
-            mainCtx.beginPath();
-            mainCtx.moveTo(x + fontSize, y); // x + radius
-            mainCtx.lineTo(
-                x + layerSpacing + (Math.pow(5, height)), // childX center
-                y + layerSpacing - fontSize // childY - radius
-            );
-            mainCtx.stroke();
-            drawBst(
-                tree.r,
-                x + layerSpacing + (Math.pow(5, height)),
-                y + layerSpacing,
-                layerSpacing,
-                height - 1
-            );
-        }
-    }
-    const bstOriginX = 360;
-    const bstOriginY = 360;
-    // spacing will be decided by height
-    const height = bst.height(searchTree);
-    const layerSpacing = 50;
-    drawBst(searchTree, bstOriginX, bstOriginY, layerSpacing, height);
+    Draw.updateCanvasStyle(mainCtx, defaultStyle);
 }
 
 window.onload = function() {
--- view/draw.js
+++ view/draw.js
@@ -44,7 +44,177 @@ const updateCanvasStyle = (ctx, params) => {
     ctx.textAlign = params.textAlign;
 };
 
+const drawStats = (ctx, target, height, nodes, step, styleParams) => {
+    // draws the basic stats to the canvas
+    ctx.font = (styleParams.fontSize * 1.5).toString() + 'px ' + styleParams.fontName;
+    ctx.fillText(
+        'Search Target: ' + target,
+        2,
+        styleParams.xMargin
+    );
+    ctx.fillText(
+        'Tree Height: ' + height,
+        2,
+        styleParams.xMargin * 2
+    );
+    ctx.fillText(
+        'Total Nodes: ' + nodes,
+        2,
+        styleParams.xMargin * 3
+    );
+    ctx.fillText(
+        'Current Search Step: ' + (step + 1),
+        2,
+        styleParams.xMargin * 4
+    );
+};
+
+const drawArrayField = (ctx, searchField, x, y, xSpacing, styleParams) => {
+    // draw the search field in array form
+    ctx.textAlign = 'center';
+    for (let i = 0; i < searchField.length; i++) {
+        ctx.fillText(
+            searchField[i],
+            x + styleParams.xMargin + (i * xSpacing),
+            y
+        );
+    }
+};
+
+const _drawSearchPath = (
+    ctx,
+    step,
+    index,
+    xSpacing,
+    xOrigin,
+    yOrigin,
+    yEnd,
+    correctPath,
+    styleParams
+) => {
+    // draw single search path
+    // searchPath is an array of indexes
+    const xEnd = styleParams.xMargin + index * xSpacing; // x of number
+    // draw line path
+    // horizontal
+    ctx.strokeStyle = '#33cc33'; // green
+    ctx.beginPath();
+    ctx.moveTo(xOrigin, yOrigin + (step * xSpacing));
+    ctx.lineTo(xEnd, yOrigin + (step * xSpacing));
+    ctx.stroke();
+    // xOrigin = xEnd;
+    // vertical part 1
+    ctx.beginPath();
+    ctx.moveTo(xEnd, yOrigin + (step * xSpacing));
+    ctx.lineTo(xEnd, yOrigin + (xSpacing * (step + 1))); // center is 1/2 fontSize and radius is fontSize
+    ctx.stroke();
+    // change color if this is an incorrect path
+    ctx.strokeStyle = correctPath ? ctx.strokeStyle : '#cc0000'; // red
+    // vertical part 2
+    ctx.beginPath();
+    ctx.moveTo(xEnd, yOrigin + (xSpacing * (step + 1)));
+    ctx.lineTo(xEnd, yEnd - (styleParams.fontSize * 1.5)); // center is 1/2 fontSize and radius is fontSize
+    ctx.stroke();
+    // draw outline circle
+    ctx.beginPath();
+    ctx.arc(xEnd, yEnd - (styleParams.fontSize/2), styleParams.fontSize, 0, 2 * Math.PI);
+    // this centers the circle on the font
+    ctx.stroke();
+};
+
+const drawSearchPath = (
+    ctx,
+    searchField,
+    searchTarget,
+    path,
+    step,
+    xSpacing,
+    yOrigin,
+    yEnd,
+    styleParams
+) => {
+    // public api to draw the array search paths
+    for (let i = 0; i <= step; i++) {
+        const correctPath = searchField[path[i]] == searchTarget;
+        const xOrigin = styleParams.xMargin + path[i - 1] * xSpacing; // center number is first
+        _drawSearchPath(
+            ctx,
+            i,
+            path[i],
+            xSpacing,
+            xOrigin,
+            yOrigin,
+            yEnd,
+            correctPath,
+            styleParams
+        );
+    }
+};
+
+const drawBst = (ctx, tree, step, path, x, y, layerSpacing, height, styleParams) => {
+    ctx.textAlign = 'center';
+    // draw current node
+    ctx.fillText(tree.k, x, y);
+    ctx.beginPath();
+    if (step !== -1 && path[step] === tree.v) {
+        // currently searching on this node
+        ctx.strokeStyle = '#0061ff'; // blue
+        ctx.lineWidth = 4;
+    }
+    ctx.arc(x, y, styleParams.fontSize, 0, 2 * Math.PI);
+    ctx.stroke();
+    // reset values
+    ctx.strokeStyle = '#000000';
+    ctx.lineWidth = 2;
+    // parents draw lines to their children
+    // then recursively draw lower nodes
+    if (tree.l) {
+        ctx.beginPath();
+        ctx.moveTo(x - styleParams.fontSize, y); // x - radius
+        ctx.lineTo(
+            x - layerSpacing - (Math.pow(5, height)), // childX center
+            y + layerSpacing - styleParams.fontSize // childY - radius
+        );
+        ctx.stroke();
+        drawBst(
+            ctx,
+            tree.l,
+            step,
+            path,
+            x - layerSpacing - (Math.pow(5, height)),
+            y + layerSpacing,
+            layerSpacing,
+            height - 1,
+            styleParams
+        );
+    }
+    if (tree.r) {
+        ctx.beginPath();
+        ctx.moveTo(x + styleParams.fontSize, y); // x + radius
+        ctx.lineTo(
+            x + layerSpacing + (Math.pow(5, height)), // childX center
+            y + layerSpacing - styleParams.fontSize // childY - radius
+        );
+        ctx.stroke();
+        drawBst(
+            ctx,
+            tree.r,
+            step,
+            path,
+            x + layerSpacing + (Math.pow(5, height)),
+            y + layerSpacing,
+            layerSpacing,
+            height - 1,
+            styleParams
+        );
+    }
+};
+
 export {
     styleParameters,
-    updateCanvasStyle
+    updateCanvasStyle,
+    drawStats,
+    drawArrayField,
+    drawSearchPath,
+    drawBst
 };
